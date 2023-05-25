979e5b2fa3ba08d43d9d2aa6833f0d95f8bd287a
Brandon Saldan <26472557+brandonsaldan@users.noreply.github.com>
2023-05-25 01:28:51 -0400

    feat: error message if localstorage is low

--- src/components/DropUpload.jsx
+++ src/components/DropUpload.jsx
@@ -6,27 +6,38 @@ import { useRouter } from 'next/router'
 export default function DropUpload() {
     const [file, setFile] = useState(null);
     let [isProcessing, setIsProcessing] = useState(false)
+    let [isOpen, setIsOpen] = useState(false)
+    let [isError, setIsError] = useState(false)
     var dna = require('dna2json');
-
-    function fileProcessing() {
-      setIsProcessing(true)
-    }
 
     const uploadToClient = (event) => {
       if (event.target.files && event.target.files[0]) {
-        const reader = new FileReader();
-        reader.onload = function (e) {
-          const fileContent = e.target.result;
-          localStorage.setItem('uploadedFileContent', fileContent);
-          setFile(event.target.files[0]);
-        };
-        reader.readAsText(event.target.files[0]);
+        const file = event.target.files[0];
+        const fileSize = file.size; // Size of the file in bytes
+    
+        // Check the available space in localStorage
+        const availableSpace = 5 * 1024 * 1024 - unescape(encodeURIComponent(JSON.stringify(localStorage))).length;
+    
+        console.log(availableSpace.toString());
+        console.log(fileSize.toString());
+
+        if (availableSpace.toString() < fileSize.toString() * 2) {
+          setIsError(true)
+          return;
+        } else {
+          const reader = new FileReader();
+          reader.onload = function (e) {
+            const fileContent = e.target.result;
+            localStorage.setItem('uploadedFileContent', fileContent);
+            setFile(event.target.files[0]);
+          };
+          reader.readAsText(file);
+        }
       }
     };
 
     const uploadToLocalStorage = async (event) => {    
-      fileProcessing();
-      const data = localStorage.getItem('uploadedFileContent');
+      setIsProcessing(true)
       dna.parse(data, function (err, snps) {
         if (err) {
           console.log(err);
@@ -39,8 +50,6 @@ export default function DropUpload() {
       setIsOpen(true);
     };
 
-    let [isOpen, setIsOpen] = useState(false)
-
     function closeModal() {
       setIsOpen(false)
     }
@@ -51,12 +60,68 @@ export default function DropUpload() {
         router.push('/results')
     }
 
+    const pageReturn = () => {
+      setIsError(false)
+  }
+
     const sampleRedirect = () => {
       router.push('/sample')
   }
 
     return (
     <>
+        <Transition appear show={isError} as={Fragment}>
+          <Dialog as="div" className="relative z-10" onClose={closeModal}>
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="fixed inset-0 bg-black bg-opacity-25" />
+            </Transition.Child>
+
+            <div className="fixed inset-0 overflow-y-auto">
+              <div className="flex min-h-full items-center justify-center p-4 text-center">
+                <Transition.Child
+                  as={Fragment}
+                  enter="ease-out duration-300"
+                  enterFrom="opacity-0 scale-95"
+                  enterTo="opacity-100 scale-100"
+                  leave="ease-in duration-200"
+                  leaveFrom="opacity-100 scale-100"
+                  leaveTo="opacity-0 scale-95"
+                >
+                  <Dialog.Panel className="w-full max-w-xl transform overflow-hidden rounded-xl bg-white p-12 text-left align-middle shadow-xl transition-all">
+                    <Dialog.Title
+                      as="h3"
+                      className="hidden sm:flex text-xl font-semibold leading-6 text-gray-900"
+                    >
+                      Alert
+                    </Dialog.Title>
+                    <div className="mt-2 grid grid-cols-1">
+                        <p className="mb-4 text-sm text-gray-500">To ensure that all processing of your data is done locally, GeneCodex uses your browser's localStorage property.</p>
+                        <p className="mb-4 text-sm text-gray-500">Unfortunately, most browsers automatically limit the amount of storage allocated to localStorage. Your file is too large for your browser's current limit.</p>
+                        <p className="mb-4 text-sm text-gray-500">Luckily, this can be easily fixed by manually setting the storage allocation. We recommend setting it to at least twice the size of your file. You can revert to your browser's default limit after your report is generated.</p>
+                        <p className="mb-4 text-sm text-gray-500">To learn how to do this for your browser, click <a className="text-blue-500 hover:text-blue-700 transition ease-in-out duration-150" href="https://github.com/brandonsaldan/codex">here</a>.</p>
+                        <button
+                            type="button"
+                            onClick={pageReturn}
+                            disabled={!file}
+                            className="mt-4 rounded-md bg-gray-800 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 transition ease-in-out duration-150"
+                        >
+                            Return
+                        </button>
+                    </div>
+                  </Dialog.Panel>
+                </Transition.Child>
+              </div>
+            </div>
+          </Dialog>
+        </Transition>
         <Transition appear show={isOpen} as={Fragment}>
           <Dialog as="div" className="relative z-10" onClose={closeModal}>
             <Transition.Child
@@ -137,7 +202,7 @@ export default function DropUpload() {
             <button
                 type="button"
                 onClick={uploadToLocalStorage}
-                disabled={!file || isProcessing}
+                disabled={isProcessing}
                 className="mt-4 rounded-md bg-gray-800 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-gray-700 transition ease-in-out duration-150"
             >
                 {isProcessing ? "Processing..." : "Continue"}
